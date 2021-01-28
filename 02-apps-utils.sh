fb30f7117ddb864ea901343211ed21664e2c9104
David Clepf <davidclepf@gmail.com>
2021-01-28 17:01:51 -0300

    Add files via upload

---
+++ 02-apps-utils.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Aplicativos e utilitários
+
+sudo pacman - S  libreoffice \
+		libreoffice-l10n-pt-br \
+		libreoffice-gnome \
+		vlc \
+		vlc-l10n \
+		chromium \
+		chromium-l10n \
+		transmission \
+		font-manager \
+		libreoffice-style-breeze \
+		gimp \
+		gnome-calculator \
+		telegram-desktop \
+		gparted \
+		krita \
