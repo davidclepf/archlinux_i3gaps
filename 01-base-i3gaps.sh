5f07bf50ec61aa47a2e69bb825dbae45b0523234
David Clepf <davidclepf@gmail.com>
2021-02-15 11:48:32 -0300

    Add files via upload

---
+++ 01-base-i3gaps.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+
+sudo pacman -S xorg \
+	xorg-server \
+	xorg-xinit \
+	xorg-apps \
+	xf86-video-vesa \
+	i3-gaps \
+	dmenu \
+	i3status \
+	i3blocks \
+	xfce4-terminal \
+	vim \
+	man-db \
+	man-pages \
+	htop \
+	alsa-utils \
+	pavucontrol \
+	pulseaudio \
+	pulseaudio-bluetooth \
+	blueman \
+	bluesz \
+	bluez-utils \
+	bluez-tools \
+	bluez-hid2hci \
+	bash-completion \
+	udiskie \
+	
+		
---
+++ 02-apps-utils.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Aplicativos e utilitários
+
+sudo pacman -S  libreoffice \
+		vlc \
+		chromium \
+		gimp \
+		gnome-calculator \
+		telegram-desktop \
+		gparted \
+		krita \
+		xcompmgr \
+		udiskie \
+		chromium \
+		thunar \
+		nitrogen \
+		feh \
+		xfce4-screenshooter \
+		xfce4-notifyd \
+		xfce4-power-manager \
+
